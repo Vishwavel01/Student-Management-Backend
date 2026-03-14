7ab2bceb13b5171a7777d89f1b523c33bb2315cf
Vishwavel01 <vishwavelmurugan01@gmail.com>
2026-03-15 00:02:35 +0530

    updated local url

--- src/main/java/com/example/StudentManagementSytem/config/CorsConfig.java
+++ src/main/java/com/example/StudentManagementSytem/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
                         .allowedOrigins(
-                                "http://localhost:4200",      // Your Localhost Frontend (e.g., React/Angular)
+                                "http://localhost:4200/students",      // Your Localhost Frontend (e.g., React/Angular)
                                 "https://vvstudentmanage.netlify.app/students" // Your Deployed Frontend URL
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
